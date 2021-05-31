6e022ad9f916524599bac38bca1a37ed3516ebd3
Madusha <madusha.wicramasinghe@zligger.com>
2021-05-31 08:08:33 +0530

    error fixed (user Registration)

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'Screen/landingPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
